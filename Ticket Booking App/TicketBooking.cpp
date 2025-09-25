cd03cb0308529bfe823cb8862c8b593167231eaf
mukeshlilawat1 <mukeshkumarlilawat1@gmail.com>
2025-09-25 19:56:52 +0530

    ticket booking app

---
+++ Ticket Booking App/TicketBooking.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <mysql.h>          // Assuming the MySQL C API header is mysql.h in your include path
+#include <mysql_error.h>
+#include <Windows.h>
+#include <sstream>
+
+using namespace std;
+
+const char *Host = "localhost";
+const char *User = "root";
+const char *Password = "12150";
+const char *Database = "mydb";
+
+class Theater
+{
+private:
+    int Seat[10][10];
+
+public:
+    Theater()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Seat[i][j] = 1;
+            }
+        }
+    }
+
+    int GetSeatStatus(int row, int Seat_Number)
+    {
+        if (row < 1 || row > 10 || Seat_Number < 1 || Seat_Number > 10)
+        {
+            return -1;
+        }
+        return Seat[row - 1][Seat_Number - 1];
+    }
+
+    void ReserveSeat(int row, int Seat_Number)
+    {
+        if (row < 1 || row > 10 || Seat_Number < 1 || Seat_Number > 10)
+        {
+            return;
+        }
+        Seat[row - 1][Seat_Number - 1] = 0;
+    }
+
+    void display()
+    {
+        cout << "  ";
+        for (int i = 0; i < 10; i++)
+        {
+            cout << " " << i + 1;
+        }
+        cout << endl;
+
+        for (int rows = 0; rows < 10; rows++)
+        {
+            cout << rows + 1 << " ";
+            for (int columns = 0; columns < 10; columns++)
+            {
+                if (Seat[rows][columns] == 1)
+                {
+                    cout << "- ";
+                }
+                else
+                {
+                    cout << "X ";
+                }
+            }
+            cout << " |" << endl;
+        }
+        cout << "----------------------" << endl;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+    MYSQL *connection;
+    connection = mysql_init(NULL);
+
+    if (!mysql_real_connect(connection, Host, User, Password, Database, 3306, NULL, 0))
+    {
+        cout << "Error : " << mysql_error(connection) << endl;
+    }
+    else
+    {
+        cout << "Logged In Database !" << endl;
+    }
+
+    Sleep(3000);
+
+    return 0;
+}
